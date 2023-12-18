e298c708dd85a95f7b4ddd8baa34655bd5ddff7d
Emmanuel Rustia Pagayonan <emmanuel_pagayonan_28@yahoo.com>
2023-12-18 21:22:59 +0800

    containerized

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python manage.py collectstatic --no-input
+
+export WORKERS=${SERVER_WORKERS:-3}
+export TIMEOUT=${WORKER_TIMEOUT:-180}
+exec gunicorn server.wsgi --workers=$WORKERS --timeout $TIMEOUT --bind 0.0.0.0:8000 --access-logfile -

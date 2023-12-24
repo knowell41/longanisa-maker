39c29f0b0869cbe7575cc7ff34eba1eb310ed6fd
Emmanuel Rustia Pagayonan <emmanuel_pagayonan_28@yahoo.com>
2023-12-24 15:32:28 +0800

    build cloud version

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ python manage.py collectstatic --no-input
 
 export WORKERS=${SERVER_WORKERS:-3}
 export TIMEOUT=${WORKER_TIMEOUT:-180}
-exec /home/pi/longanisa-maker/env/bin/gunicorn server.wsgi --workers=$WORKERS --timeout $TIMEOUT --bind 0.0.0.0:8000 --access-logfile -
+exec gunicorn server.wsgi --workers=$WORKERS --timeout $TIMEOUT --bind 0.0.0.0:8000 --access-logfile -

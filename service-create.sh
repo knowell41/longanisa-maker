3325b25090006cf625357cce3de5e18520df5884
Emmanuel Rustia Pagayonan <emmanuel_pagayonan_28@yahoo.com>
2023-12-24 17:37:26 +0800

    mv to cp

--- service-create.sh
+++ service-create.sh
@@ -1,4 +1,4 @@
-sudo mv sausageparty.service /etc/systemd/system/
+sudo cp sausageparty.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable sausageparty
 sudo systemctl start sausageparty
